54f4fc568cfefaaca1a1fc64c32494d9b22a1fbd
Puntzi <70822492+Puntzi@users.noreply.github.com>
2023-03-31 12:13:19 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -16,7 +16,7 @@ RegisterNetEvent('pnt_panicButton:server:sendAlert', function(policeCoords)
     local xPlayers = ESX.GetExtendedPlayers()
     
     for _, player in pairs(xPlayers) do
-        if player.job.name == Config.PoliceName --[[ and not (player.source == xPlayer.source)  ]]then
+        if player.job.name == Config.PoliceName and not (player.source == xPlayer.source) then
             lib.callback('pnt_panicButton:client:reciveAlert', player.source, false, policeCoords, xPlayer.getName())
         end
     end
