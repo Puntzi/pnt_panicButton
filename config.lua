933400e2fcb0769f5c2f0c8751fa30355609fd0b
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2024-06-25 18:06:57 +0200

    add multi police jobs

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = Config or {}
 
-Config.PoliceName = 'police'
+Config.PoliceJobs = {['police'] = true, ['police2'] = true, ['bcso'] = true}
 
 -- Here you can look for keys to change the buttons https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
 -- Remember if you change it here and it doesn't change ingame check in ingame settings to change it
--- server/server.lua
+++ server/server.lua
@@ -2,22 +2,20 @@ lib.locale()
 
 lib.callback.register('pnt_panicButton:isPolice', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.job.name == Config.PoliceName then
-        return true
-    else
-        return false
-    end
+    return Config.PoliceJobs[xPlayer.job.name] == true
 end)
 
 RegisterNetEvent('pnt_panicButton:server:sendAlert', function(policeCoords)
     local xPlayer = ESX.GetPlayerFromId(source)
     if not xPlayer then return end
 
-    local xPlayers = ESX.GetExtendedPlayers()
-    
-    for _, player in pairs(xPlayers) do
-        if player.job.name == Config.PoliceName and not (player.source == xPlayer.source) then
-            lib.callback('pnt_panicButton:client:reciveAlert', player.source, false, policeCoords, xPlayer.getName())
+    local src = xPlayer.source
+    local name = xPlayer.getName()
+
+    for job,_ in pairs(Config.PoliceJobs) do
+        local xPlayers = ESX.GetExtendedPlayers('job', job)
+        for id, player in pairs(xPlayers) do
+            lib.callback('pnt_panicButton:client:reciveAlert', player.source, false, policeCoords, name, src)
         end
     end
 end)
